172ea52356af824dc7d318032ac2fc8f87f2bc12
Savin Pathirana <savinpathirana@gmail.com>
2026-03-26 15:52:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 require('dotenv').config();
+require('./config/db');
 
 const app = express();
 
