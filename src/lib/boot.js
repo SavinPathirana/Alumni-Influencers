7084ee12e5d46c5b79eff80993cfdc3e2d0b7b96
Savin Pathirana <savinpathirana@gmail.com>
2026-03-31 09:08:22 +0530

    Update boot.js

--- src/lib/boot.js
+++ src/lib/boot.js
@@ -1,25 +1,14 @@
 'use strict'
 
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var fs = require('node:fs');
 var path = require('node:path');
 
-/**
- * Auto-loader for controller subdirectories.
- * Scans the controllers/ directory for subfolders, each containing an index.js
- * that exports an Express Router. Mounts each router at /api/<folder-name>.
- * 
- * Adapted from the MVC boilerplate's lib/boot.js convention.
- */
-module.exports = function(parent, options){
+module.exports = function (parent, options) {
   var dir = path.join(__dirname, '..', 'controllers');
   var verbose = options.verbose;
 
-  fs.readdirSync(dir).forEach(function(name){
+  fs.readdirSync(dir).forEach(function (name) {
     var file = path.join(dir, name);
 
     //Skip if it is not a directory
@@ -27,10 +16,10 @@ module.exports = function(parent, options){
 
     verbose && console.log('\n   %s:', name);
 
-    //Require the controller's index.js which exports a Router
+    //Require the controller's index.js
     var router = require(file);
 
-    //Mount the router under /api/<controller-name>
+    //Mount the router
     var mountPath = '/api/' + name;
     parent.use(mountPath, router);
 
