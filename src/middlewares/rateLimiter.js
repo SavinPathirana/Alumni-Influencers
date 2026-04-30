bfcd684783880d425fde46ace869c38be985a1af
Savin Pathirana <savinpathirana@gmail.com>
2026-04-30 06:47:12 +0530

    rate limit window fix after testing

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,10 +1,10 @@
 const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, //1 hour in milliseconds
-    max: 1500, //Limit each IP to 1500 requests to support dashboard charts
+    windowMs: 15 * 60 * 1000, //15 minutes in milliseconds
+    max: 100, //Limit each IP to 100 requests per 15 minutes
     message: {
-        error: 'Too many requests from this IP. The AR Client is restricted to 100 requests per hour. Please try again later.'
+        error: 'Too many requests from this IP. The AR Client is restricted to 100 requests per 15 minutes. Please try again later.'
     },
     standardHeaders: true, //Return info in the `RateLimit-*` headers
     legacyHeaders: false, //Disable the `X-RateLimit-*` headers
